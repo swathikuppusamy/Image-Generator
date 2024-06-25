ddf3f91fc6f747beeab26e82cb8aa0752725b821
swathikuppusamy <swathikuppusamy2005@gmail.com>
2024-06-25 21:51:00 +0530

    client - commit

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,43 +1,40 @@
-import React from 'react'
-import { SearchOutlined } from '@mui/icons-material'
-import styled from 'styled-components'
- const SearchBarContainer = styled.div`
-  max-width:550px;
-  display:flex;
-  width:90%;
-  border:1px solid ${({theme})=>theme.text_secondary+90};
-  color:${({theme})=>theme.text_primary};
-  border-radius:8px;
+
+import React from "react";
+import { SearchOutlined } from "@mui/icons-material";
+import styled from "styled-components";
+
+const SearchBarContainer = styled.div`
+  max-width: 550px;
+  display: flex;
+  width: 90%;
+  border: 1px solid ${({ theme }) => theme.text_secondary + 90};
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 8px;
   padding: 12px 16px;
-  cursor:pointer;
-  gap:6px;
-  align-items:center;
-  
+  cursor: pointer;
+  gap: 6px;
+  align-items: center;
+`;
 
-
-
-
- `;
-const SearchBar = () => {
-  return <SearchBarContainer>
-    <SearchOutlined />
-      <input 
-      placeholder='Search by entering a prompt or name . . .'
-      style={{
-        border:"none",
-        outline:"none",
-        width:"100%",
-        color:"inherit",
-        background: "transparent",
-        fontSize:"18px",
-        
-      }}
-      
-      
-      
-      
+const SearchBar = ({ search, setSearch }) => {
+  return (
+    <SearchBarContainer>
+      <SearchOutlined />
+      <input
+        placeholder="Search with prompt or name . . ."
+        style={{
+          border: "none",
+          outline: "none",
+          width: "100%",
+          color: "inherit",
+          fontSize: "16px",
+          background: "transparent",
+        }}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
-  </SearchBarContainer>
-}
+    </SearchBarContainer>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import styled from "styled-components";
 import GenerateImageForm from "../components/GenerateImageForm";
